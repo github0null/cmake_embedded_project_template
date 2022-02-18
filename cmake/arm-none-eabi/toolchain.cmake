07a451888b2d4bdb3df254910bbed37968f50abf
github0null <2584456014@qq.com>
2022-02-18 10:49:25 +0800

    update toolchain config

--- cmake/arm-none-eabi/toolchain.cmake
+++ cmake/arm-none-eabi/toolchain.cmake
@@ -1,11 +1,11 @@
 # Toolchain settings
 set(CMAKE_C_COMPILER    arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER  arm-none-eabi-g++)
-set(AS                  arm-none-eabi-as)
-set(AR                  arm-none-eabi-ar)
-set(OBJCOPY             arm-none-eabi-objcopy)
-set(OBJDUMP             arm-none-eabi-objdump)
-set(SIZE                arm-none-eabi-size)
+set(CMAKE_ASM_COMPILER  arm-none-eabi-gcc)
+set(CMAKE_LINKER        arm-none-eabi-gcc)
+set(CMAKE_AR            arm-none-eabi-ar)
+set(CMAKE_OBJCOPY       arm-none-eabi-objcopy)
+set(CMAKE_OBJDUMP       arm-none-eabi-objdump)
 
 set(CMAKE_C_COMPILER_WORKS TRUE)
 set(CMAKE_CXX_COMPILER_WORKS TRUE)
