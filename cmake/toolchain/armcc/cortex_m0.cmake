faa51900a6c7934350fdf0f89969cb635dcc6cf8
github0null <2584456014@qq.com>
2022-02-21 14:12:24 +0800

    update

--- cmake/toolchain/armcc/cortex_m0.cmake
+++
@@ -1,8 +0,0 @@
-# Name of the target
-set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR cortex-m0)
-
-set(MCPU_FLAGS "--cpu Cortex-M0")
-set(VFP_FLAGS "")
-
-include(${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake)
